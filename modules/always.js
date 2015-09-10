12b3a20267c5ecdebbd39b26db8d9f1c630848df
Cleber de Moraes Goncalves <cleber.programmer@hotmail.com>
2015-09-10 13:32:55 +0000

    refatoracao: documentacao

--- modules/always.js
+++ modules/always.js
@@ -1,9 +1,37 @@
+/**
+ * $always
+ * 
+ * Retorna uma funcao com o valor dado
+ * 
+ * @module $always
+ * @author Cleber de Moraes Goncalves <cleber.programmer>
+ * @example
+ * 
+ *        $always('cleber.programmer');
+ * 
+ */
 this.Ninja.module('$always', [], function () {
   
+  /**
+   * Retorna uma funcao com o valor dado
+   * 
+   * @public
+   * @method always
+   * @param {Array|Boolean|Date|Funcation|Null|Object|String|Undefined} a Valor que sera rotornado
+   * @return Funcao que quando executado retorna o valor passado
+   * @example
+   *
+   *        $always('cleber.programmer');
+   * 
+   */
   function always(a) {
-    return function () { return a; }
+    return function () { return a; };
   }
   
+  /**
+   * Revelacao do modulo $always, encapsulando a visibilidade das funcoes
+   * privadas
+   */
   return always;
   
 });
--- modules/equal.js
+++ modules/equal.js
@@ -1,7 +1,7 @@
 /**
  * $equal
  * 
- * Retorna true se seus argumentos são equivalentes, False contrário a funcao curry
+ * Retorna True se seus argumentos são equivalentes, False contrário a funcao curry
  * 
  * @module $equal
  * @author Cleber de Moraes Goncalves <cleber.programmer>
@@ -12,10 +12,27 @@
  */
 this.Ninja.module('$equal', ['$curry'], function ($curry) {
   
+  /**
+   * Retorna True se seus argumentos são equivalentes, False contrário
+   * 
+   * @public
+   * @method equal
+   * @param {Array|Boolean|Date|Funcation|Null|Object|String|Undefined} a Primeiro valor
+   * @param {Array|Boolean|Date|Funcation|Null|Object|String|Undefined} b Segundo valor
+   * @return Resposata da comparacao dos dois argumentos
+   * @example
+   * 
+   *        $equal('cleber.programmer', 'cleber.programmer');
+   * 
+   */
   function equal(a, b) {
     return a == b;
   }
   
+  /**
+   * Revelacao do modulo $equal, encapsulando a visibilidade das funcoes
+   * privadas
+   */
   return $curry(equal);
   
 });
