0daab3baf3a3ca24bea658334d260f10da240a23
Cleber de Moraes Goncalves <cleber.programmer@hotmail.com>
2015-09-11 16:24:51 +0000

    refatoracao do web component bbz-error

--- components/bbz-error.js
+++ components/bbz-error.js
@@ -1,6 +1,7 @@
 this.Ninja([
   
   '$compose',
+  '$curry',
   '$dispatcher',
   '$fileRequest',
   '$format',
@@ -9,9 +10,23 @@ this.Ninja([
   '$template',
   '$webComponent'
 
-], function ($compose, $dispatcher, $fileRequest, $format, $pick, $prop, $template, $webComponent, _) {
+], function ($compose, $curry, $dispatcher, $fileRequest, $format, $pick, $prop, $template, $webComponent, _) {
   
-  function dispatcherHook(method, element) {
+  /**
+   * Hook method para os eventos de attached e detached, sendo que
+   * ambos so mudam a chamada do dispatacher. Um adicionando uma esculta a
+   * um canal e outro desvinculando
+   * 
+   * @private
+   * @method hook
+   * @param {Node} element Web Componente bbz-error
+   * @param {String} method Nome da funcao do dispatcher, sendo on ou off
+   * @example
+   * 
+   *        hook(element, 'on');
+   * 
+   */
+  function hook(element, method) {
     $dispatcher[method](
       $format('{0}:input:error', [$prop('uuid', element)]),
       element.setState,
@@ -19,16 +34,28 @@ this.Ninja([
     );
   }
   
+  /**
+   * Criacao do Web Componente bbz-error, responsavel pela exibicao
+   * de erros
+   */
   $webComponent('bbz-error', {
     
-    attached: function (element) {
-      dispatcherHook('on', element);
-    },
+    /**
+     * Quando o Web Component bbz-error for adicionado ao DOM,
+     * sera assinado as escultas para os eventos {0}:input:error
+     */
+    attached: $curry(hook)(_, 'on'),
     
-    detached: function (element) {
-      dispatcherHook('off', element);
-    },
+    /**
+     * Quando o Web Component bbz-error for removido do DOM,
+     * sera desvinculado a assinatura {0}:input:error evitando sobre carga
+     * ao dispatcher
+     */
+    detached: $curry(hook)(_, 'off'),
     
+    /**
+     * Quando alterado o stado do componente, o template sera executado
+     */
     template: function (element, data, render) {
       $fileRequest('./templates/bbz-error.html', $compose(render, $template(_, data)));
     }
