636f49fd403bf97b770bf311161ff13c359914b2
Cleber de Moraes Goncalves <cleber.programmer@hotmail.com>
2015-09-28 15:27:43 +0000

    [form.Ninja.JS] refatoracao: inclusao de modulos para o bower

--- modules/innerHTML.js
+++
@@ -1,9 +0,0 @@
-this.Ninja.module('$innerHTML', ['$curry'], function ($curry) {
-
-  function innerHTML(element, html) {
-    element.shadowRoot.innerHTML = html;
-  }
-
-  return $curry(innerHTML);
-
-});
